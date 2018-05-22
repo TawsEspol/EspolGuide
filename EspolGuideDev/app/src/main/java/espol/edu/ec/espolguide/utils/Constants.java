62e5e41ebce281a0cd43fb84ceee9b23e58bbf9b
Fabricio Layedra <flayedra@espol.edu.ec>
2018-05-22 04:44:39 -0500

    Authentication service linked. Start layout linked with activity_map. Better design left.

--- EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/utils/Constants.java
+++ EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/utils/Constants.java
@@ -9,13 +9,20 @@ import android.net.ConnectivityManager;
 
 public class Constants {
     public final static String IP_ = "172.19.25.101:8000";
-
-
     public final static String SERVER_URL = "http://" + IP_;
     public final static double ESPOL_CENTRAL_LONG = -79.96575;
     public final static double ESPOL_CENTRAL_LAT = -2.14630;
     public final static int START_ZOOM = 18;
     public final static int ZOOM_MAX = 20;
+
+    public final static String NAMESPACE = "http://tempuri.org/";
+    public final static String URL="https://ws.espol.edu.ec/saac/wsGuide.asmx";
+    public final static String AUTH_METHOD_NAME = "autenticacion";
+    public final static String AUTH_SOAP_ACTION = "http://tempuri.org/autenticacion";
+
+    public final static String SOAP_HEADER = "GTSIAuthSoapHeader";
+    public final static String USER_SOAP_HEADER = "taws";
+    public final static String KEY_SOAP_HEADER = "jmMH2VY17PHPmUq2w7E2o7W4I2O9d16u";
 
     public static boolean isNetworkAvailable(final Context context) {
         final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
