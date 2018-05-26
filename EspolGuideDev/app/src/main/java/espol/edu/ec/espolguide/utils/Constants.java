00ed2d456ba8011223e7b15e58a135c562701227
Fabricio Layedra <flayedra@espol.edu.ec>
2018-05-26 14:00:48 -0500

    First conflicts resolving

--- EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/utils/Constants.java
+++ EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/utils/Constants.java
@@ -8,7 +8,8 @@ import android.net.ConnectivityManager;
  */
 
 public class Constants {
-    public final static String IP_ = "192.168.0.7:8080";
+    public final static String IP_ = "";
+
     public final static String SERVER_URL = "http://" + IP_;
     public final static int CLOSE_ZOOM = 18;
     public final static int FAR_AWAY_ZOOM = 15;
@@ -16,6 +17,15 @@ public class Constants {
     public final static String FROM_DESTINATION = "from_destination";
     public final static int REQUEST_CODE = 1;
 
+
+    public final static String NAMESPACE = "http://tempuri.org/";
+    public final static String URL="https://ws.espol.edu.ec/saac/wsGuide.asmx";
+    public final static String AUTH_METHOD_NAME = "autenticacion";
+    public final static String AUTH_SOAP_ACTION = "http://tempuri.org/autenticacion";
+
+    public final static String SOAP_HEADER = "GTSIAuthSoapHeader";
+    public final static String USER_SOAP_HEADER = "taws";
+    public final static String KEY_SOAP_HEADER = "jmMH2VY17PHPmUq2w7E2o7W4I2O9d16u";
     public static boolean isNetworkAvailable(final Context context) {
         final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
         return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
