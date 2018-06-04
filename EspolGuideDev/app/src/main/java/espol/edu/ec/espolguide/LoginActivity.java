057d60d44bf458d189a3dc6234b985ab5ca6e345
Fabricio Layedra <flayedra@espol.edu.ec>
2018-06-04 17:26:54 -0500

    [Sprint 3] System Authentication: Login with Facebook Task#99 Login implementation, main feature.

--- EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/LoginActivity.java
+++ EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/LoginActivity.java
@@ -1,19 +1,30 @@
 package espol.edu.ec.espolguide;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.mapbox.mapboxsdk.Mapbox;
 
 import java.util.Observable;
 import java.util.Observer;
 
+import espol.edu.ec.espolguide.utils.Constants;
 import espol.edu.ec.espolguide.viewModels.LoginViewModel;
+import espol.edu.ec.espolguide.viewModels.MapViewModel;
 
 /**
  * Created by fabricio on 19/05/18.
@@ -22,34 +33,66 @@ import espol.edu.ec.espolguide.viewModels.LoginViewModel;
 public class LoginActivity extends AppCompatActivity implements Observer {
     ViewHolder viewHolder;
     LoginViewModel viewModel;
+    private CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.access_token));
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.start);
-        this.viewHolder = new ViewHolder();
+        this.viewHolder = new ViewHolder(this);
         this.viewModel = new LoginViewModel(this);
         this.viewModel.addObserver(this);
     }
 
-    public class ViewHolder{
+    public class ViewHolder {
         public EditText username;
         public EditText password;
         public Button authBtn;
+        public LoginButton fbAuthBtn;
+        private Activity ctx;
 
-        public ViewHolder(){
+        public ViewHolder(Activity ctx) {
+            this.ctx = ctx;
             findViews();
             setAuthButtonListener();
+            setFbLogin();
         }
 
-        private void findViews(){
+        private void setFbLogin() {
+
+            callbackManager = CallbackManager.Factory.create();
+            fbAuthBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    Intent intent;
+                    intent = new Intent(ctx, MapActivity.class);
+                    ctx.startActivity(intent);
+                    ctx.finish();
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+
+                @Override
+                public void onError(FacebookException e) {
+
+                }
+            });
+        }
+
+        private void findViews() {
             username = (EditText) findViewById(R.id.username);
             password = (EditText) findViewById(R.id.password);
             authBtn = (Button) findViewById(R.id.buttonAuth);
+            fbAuthBtn = (LoginButton) findViewById(R.id.fbAuthBtn);
         }
 
-        private void setAuthButtonListener(){
+        private void setAuthButtonListener() {
             this.authBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -57,48 +100,19 @@ public class LoginActivity extends AppCompatActivity implements Observer {
                 }
             });
         }
-    }
 
-    public ViewHolder getViewHolder(){
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    public ViewHolder getViewHolder() {
         return this.viewHolder;
     }
 
     @Override
-    public void update(Observable o, Object arg) {
-        String message = (String)arg;
-        if (message == viewModel.AUTH_REQUEST_STARTED) {
+    public void update(Observable observable, Object o) {
 
-        }
-        if (message == viewModel.AUTH_REQUEST_SUCCEED) {
-            Intent intent = new Intent(this, MapActivity.class);
-            this.startActivity(intent);
-            this.viewHolder.password.setText("");
-            this.finish();
-        }
-        if (message == viewModel.AUTH_REQUEST_FAILED_CONNECTION) {
-            LoginActivity.this.runOnUiThread(new Runnable() {
-                public void run() {
-                    Toast.makeText(LoginActivity.this, getResources().getString(R.string.failed_connection_msg),
-                            Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-        if (message == viewModel.AUTH_WRONG_CREDENTIALS) {
-            this.viewHolder.password.setText("");
-            LoginActivity.this.runOnUiThread(new Runnable() {
-                public void run() {
-                    Toast.makeText(LoginActivity.this, getResources().getString(R.string.wrong_credentials_msg),
-                            Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-        if (message == viewModel.AUTH_REQUEST_FAILED_HTTP) {
-            LoginActivity.this.runOnUiThread(new Runnable() {
-                public void run() {
-                    Toast.makeText(LoginActivity.this, getResources().getString(R.string.http_error_msg),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
     }
 }
