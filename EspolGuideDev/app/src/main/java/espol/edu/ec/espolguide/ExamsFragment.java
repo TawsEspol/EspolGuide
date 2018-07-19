03ef1f4f4c7fc75db0fca25471a46f44eec65c92
Fabricio Layedra <flayedra@espol.edu.ec>
2018-07-19 11:55:49 -0500

    Adding Lectures and Exams fragments

---
+++ EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/ExamsFragment.java
@@ -0,0 +1,32 @@
+package espol.edu.ec.espolguide;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import espol.edu.ec.espolguide.utils.SessionHelper;
+import espol.edu.ec.espolguide.utils.User;
+import espol.edu.ec.espolguide.utils.assync.SubjectsSoapHelper;
+
+/**
+ * Created by fabricio on 07/07/18.
+ */
+
+public class ExamsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_exams, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String username = SessionHelper.getEspolUsername(getView().getContext());
+        User user = new User(getView().getContext(), username, false, getView().findViewById(R.id.exams_container) );
+        new SubjectsSoapHelper().execute(user);
+    }
+}
