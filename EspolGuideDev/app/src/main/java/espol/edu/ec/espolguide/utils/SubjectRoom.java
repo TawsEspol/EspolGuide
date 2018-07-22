194c51fc7c86c545dd054ebffc67af60ec00f43f
Fabricio Layedra <flayedra@espol.edu.ec>
2018-07-21 19:37:31 -0500

    [REFACTORING] Class for modeling Subject to put at scrollable

---
+++ EspolGuideDev/app/src/main/java/espol/edu/ec/espolguide/utils/SubjectRoom.java
@@ -0,0 +1,75 @@
+package espol.edu.ec.espolguide.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by fabricio on 14/07/18.
+ */
+
+public class SubjectRoom extends android.support.v7.widget.AppCompatButton {
+    private String day;
+    private String place;
+
+    public SubjectRoom(Context ctx, String day, String place){
+        super(ctx);
+        this.day = day;
+        this.place = place;
+    }
+
+    public String toString(){
+        return (this.day +" | "+this.place);
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+
+}
+
